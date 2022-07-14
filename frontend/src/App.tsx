0b758f64ca2e47261509e62b1a08b37c83659fbd
Elton Luiz <eltonluiz12042012@gmail.com>
2022-07-14 20:44:37 -0300

    Add components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificatioButton"
+import SalesCard from "./components/SalesCard/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton/>
+      <Header />
+        <main>
+          <section id="sales">
+            <div className="dsmeta-container">
+              <SalesCard/>
+             </div>
+          </section>
+        </main>
     </>
   )
   
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+
+import logo from '../../assets/img/logo.svg';
+import './style.css';
+
+
+function Header() {
+    return (
+        <header id="sales">
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="Logo do site"/>
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.linkedin.com/in/elton-luiz-santos-do-franco-dpo/" target="_blank">Elton Luiz</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
