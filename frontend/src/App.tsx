c1b975ddb4cb879e64e4c70d7ee6cd6f0346aacb
Elton Luiz <eltonluiz12042012@gmail.com>
2022-07-13 22:02:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
-
+import NotificationButton from "./components/NotificatioButton"
 
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton/>
+    </>
   )
   
 }
