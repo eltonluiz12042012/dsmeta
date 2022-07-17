ea4d3e016134399a29d5fbcf54b551871f95e360
Elton Luiz <eltonluiz12042012@gmail.com>
2022-07-17 11:12:07 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
+import { ToastContainer } from 'react-toastify';
+
 import Header from "./components/Header"
 import NotificationButton from "./components/NotificatioButton"
 import SalesCard from "./components/SalesCard/SalesCard"
 
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
         <main>
           <section id="sales">
